ec54e9f75542bbe264b35f36ea335315160e8444
Josue Rojas <josue.aripez@uabc.edu.mx>
2023-05-01 13:19:46 -0700

    ver1.0

---
+++ Prueba.py
@@ -0,0 +1,34 @@
+#Probando Visual Studio y Git Hub
+
+import os
+
+def Escribe_Mensaje():
+    palabra = input("Ingresa una palabra a imprimir: ")
+    print(palabra)
+    
+def Imprime_Mensaje():
+    print("Hola Mundo")
+
+def menu():
+    print("-------MENU-------")
+    print("1.- Imprimir un mensaje ya escirto ")
+    print("2.- Escribir t propio mensaje ")
+    print("0.- Salir ")
+    
+while True:
+    menu()
+    
+    op = int(input("Eliga una opcion: "))
+    
+    if op == 1:
+        Imprime_Mensaje()
+        input("Enter para borrar")
+        os.system('cls')
+        continue
+    elif op == 2:
+        Escribe_Mensaje()
+        input("Enter para borrar")
+        os.system('cls')
+        continue
+    elif op == 0:
+        break
