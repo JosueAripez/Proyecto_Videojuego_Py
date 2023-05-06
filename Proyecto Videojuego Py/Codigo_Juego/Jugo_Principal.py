a5635cf78cb898a7662b135d194fadfd9a5111e5
JosueAripez <josue.aripez@uabc.edu.mx>
2023-05-05 17:16:23 -0700

    Cambios Jos

---
+++ Proyecto Videojuego Py/Codigo_Juego/Jugo_Principal.py
@@ -0,0 +1,11 @@
+import pygame, sys
+
+pygame.init()
+
+size = (600,400)
+screen = pygame.display.set_mode(size)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
