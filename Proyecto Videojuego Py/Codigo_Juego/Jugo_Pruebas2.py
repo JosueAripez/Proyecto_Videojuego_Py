b95c4b9961a8d8075faa597fbed6658bd8a9a0ea
iRed72 <josue.aripez@uabc.edu.mx>
2023-05-15 10:41:27 -0700

    Cambios

--- Proyecto Videojuego Py/Codigo_Juego/Jugo_Pruebas2.py
+++ Proyecto Videojuego Py/Codigo_Juego/Jugo_Pruebas2.py
@@ -8,6 +8,10 @@ from tkinter import messagebox
 
 def Abrir_Ventana_Menu():
     print("")
+    
+    def Volver():
+        Ventana_Menu.destroy()
+    
     Ventana_Menu = Toplevel()
     Ventana_Menu.title("4 PAISES 1 BANDERA")
     Ventana_Menu.iconbitmap("Proyecto Videojuego Py\imagenes\icono.ico")
@@ -19,6 +23,10 @@ def Abrir_Ventana_Menu():
 
     Selec_Con = LabelFrame(Ventana_Menu, text="SELECCIONA UN CONTINENTE", width=1160, height=760, background="black", fg="white", font=("Verdana", 20))
     Selec_Con.place(x=20, y=20)
+
+    Btn_Volver = Button(Ventana_Menu, text="VOLVER", width=10, height=2, background="white", fg="black", borderwidth=5, relief="raised", font=("Verdana", 10), command=Volver)
+    Btn_Volver.place(x=530, y=700)
+    
     Ventana_Menu.mainloop()
     Ventana_Menu.state(newstate = "normal")
     Ventana_Principal.state(newstate = "withdraw")
@@ -47,19 +55,14 @@ Ventana_Principal.config(cursor="hand2")
     
 lbl_Titulo = Label(Ventana_Principal, text="4 Paises 1 Bandera", fg="blue", bg="yellow", font=("Verdana", 80),  borderwidth=5, relief="groove")
 lbl_Titulo.place(x=75, y=40)
-    
 Btn_Comenazar = Button(Ventana_Principal, text="COMENZAR", width=30, height=3, background="white", fg="black", borderwidth=5, relief="raised", font=("Verdana", 10), command=Abrir_Ventana_Menu)
 Btn_Comenazar.place(x=450, y=300)
-    
 Btn_Record = Button(Ventana_Principal, text="RECORDS", width=30, height=3, background="white", fg="black", borderwidth=5, relief="raised", font=("Verdana", 10), command=Records)
 Btn_Record.place(x=450, y=380)
-    
 Btn_Salir = Button(Ventana_Principal, text="SALIR", width=30, height=3, background="white", fg="black", borderwidth=5, relief="raised", font=("Verdana", 10), command=Salir)
 Btn_Salir.place(x=450, y=460)
-    
 lbl_Create = Label(Ventana_Principal, text="Propiedad Intelectual y Creativa de: Jose Abrham Beristain Navarro y Josue Franciso Rojas Aripez", fg="white", bg="black", font=("Verdana", 10),  borderwidth=5)
 lbl_Create.place(x=5, y=770)
-
 # --- Fin ---
     
 Ventana_Principal.mainloop()
