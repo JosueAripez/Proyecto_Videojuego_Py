240e24b995b857be3dc707e3de785794a2a2a6a9
Abraham_Beristain <jose.beristain@uabc.edu.mx>
2023-05-22 08:52:57 -0700

    Abraham

--- Proyecto Videojuego Py/Codigo_Juego/Jugo_Pruebas5.py
+++ Proyecto Videojuego Py/Codigo_Juego/Jugo_Pruebas5.py
@@ -3,20 +3,43 @@
 import random
 from tkinter import *
 
-root=Tk()
-root.title("Ejemplo")
+ventana = Tk()
+ventana.title("Ejemplo")
+ventana.geometry("500x500")
+ventana.resizable(0,0)
 
 
-frame=Frame(root)
-frame.pack()
+lbl_Titulo = Label(ventana, text="IMAGENES ALEATORIAS:")
+lbl_Titulo.place(x=10, y=10)
 
-Label(frame, text="IMAGENES ALEATORIAS:", width=50).pack()
 
+#Abraham: Para imagenes aleatorias
 imagenes = ["Proyecto videojuego Py/imagenes/Banderas/Africa/Angola-Bandera-Africa.png", "Proyecto videojuego Py/imagenes/Banderas/Africa/Argelia-Bandera-Africa.png", "Proyecto videojuego Py/imagenes/Banderas/Africa/Benin-Bandera-Africa.png", "Proyecto videojuego Py/imagenes/Banderas/Africa/Botsuana-Bandera-Africa.png", "Proyecto videojuego Py/imagenes/Banderas/Africa/Burkina-Faso-Bandera-Africa.png"]
-palabra = random.choice(imagenes)
-foto=PhotoImage(file=palabra)
-Label(root, image=foto).pack()
+bandera = random.choice(imagenes)
+foto=PhotoImage(file=bandera)
+lbl_Bandera = Label(ventana, image=foto)
+lbl_Bandera.place(x=100, y=50)
+
+
+#Abraham: Texto de los botones aleatorio
+paises = ["Angola", "Argelia", "Benin", "Botsuana", "Burkina Faso"]
+
+texto = random.choice(paises)
+btn_Opcion1 = Button(ventana, text=texto)
+btn_Opcion1.place(x=50, y=300)
+
+texto = random.choice(paises)
+btn_Opcion2 = Button(ventana, text=texto)
+btn_Opcion2.place(x=200, y=300)
+
+texto = random.choice(paises)
+btn_Opcion3 = Button(ventana, text=texto)
+btn_Opcion3.place(x=50, y=400)
+
+texto = random.choice(paises)
+btn_Opcion4 = Button(ventana, text=texto)
+btn_Opcion4.place(x=200, y=400)
 
 
 
-root.mainloop()
+ventana.mainloop()
