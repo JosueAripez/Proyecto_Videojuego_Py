59906896785c61622f145e20aad509c16bda5c1d
JosueAripez <josue.aripez@uabc.edu.mx>
2023-05-23 11:17:13 -0700

    Cambios

--- Proyecto Videojuego Py/Codigo_Juego/Jugo_Pruebas4.py
+++ Proyecto Videojuego Py/Codigo_Juego/Jugo_Pruebas4.py
@@ -15,13 +15,13 @@ ventana_Oceania.geometry("+75+10")
 ventana_Oceania.config(cursor="hand2")
 
 
-lbl_Titulo = Label(ventana_Oceania, text="IMAGENES ALEATORIAS:", background="black", fg="white")
+lbl_Titulo = Label(ventana_Oceania, text="OCEANIA:", background="black", fg="white")
 lbl_Titulo.place(x=10, y=10)
 
 
 # --- Imagenes Aleatorias ---
 
-imagenes = ["Proyecto Videojuego Py\imagenes\Banderas\Oceania\Australia.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Fiyi.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Islas-Marshall.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Islas-Solomon.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Kiribati.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Micronesia.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Nauru.png","Proyecto Videojuego Py\imagenes\Banderas\Oceania\Nueva-Zelanda.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Palaos.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Papua-Nueva-Guinea.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Samoa.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Tonga.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Tuvalu.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Vanuatu.png"]
+imagenes = ["Proyecto Videojuego Py\imagenes\Banderas\Oceania\Australia.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Fiyi.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Islas-Marshall.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Islas-Solomon.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Kiribati.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Micronesia.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\BNauru.png","Proyecto Videojuego Py\imagenes\Banderas\Oceania\BNueva-Zelanda.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Palaos.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Papua-Nueva-Guinea.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Samoa.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Tonga.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Tuvalu.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Vanuatu.png"]
 bandera = random.choice(imagenes)
 foto=PhotoImage(file=bandera)
 lbl_Bandera = Label(ventana_Oceania, image=foto)
