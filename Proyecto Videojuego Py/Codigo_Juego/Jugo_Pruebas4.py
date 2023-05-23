1a2f2fe41550e0e8735fdf304303a415f6d5ba66
JosueAripez <josue.aripez@uabc.edu.mx>
2023-05-23 10:46:38 -0700

    Cambios

--- Proyecto Videojuego Py/Codigo_Juego/Jugo_Pruebas4.py
+++ Proyecto Videojuego Py/Codigo_Juego/Jugo_Pruebas4.py
@@ -1,45 +1,47 @@
 # --- Importando ---
 
-import tkinter as tk
+import random
 from tkinter import *
-from PIL import ImageTk, Image
 
-# --- Ventana 1 (Inicio) ---
+# ---  Ventana Oceania ---
 
-def ventana_inicio():
-    global ventana_inicio
-    ventana_inicio = Tk()
-    
-    ventana_inicio.iconbitmap('Proyecto Videojuego Py\imagenes\icono.ico')
-    ventana_inicio.title('4 PAISES UNA BANDERA')
-    ventana_inicio.geometry('1000x700') # Ancho x Alto
-    ventana_inicio.resizable(0,0)
-    
-    Label(ventana_inicio, text="4 Paises Una Bandera", fg="medium blue", font=("Arial",50)).grid(pady=5, row=0, column=2) 
+ventana_Oceania= Tk()
+ventana_Oceania.title("4 PAISES 1 BANDERA")
+ventana_Oceania.iconbitmap("Proyecto Videojuego Py\imagenes\icono.ico")
+ventana_Oceania.resizable(0,0)
+ventana_Oceania.configure(background="black")
+ventana_Oceania.geometry("1200x650")
+ventana_Oceania.geometry("+75+10")
+ventana_Oceania.config(cursor="hand2")
 
-    Button(ventana_inicio, text="Comenzar", width=20, command= ventana_menu).grid(pady=10, padx=10, row=2, column=2, )#.place(relx=0.5, rely=0.5)
-    
-    ventana_inicio.mainloop()
-    
-def ventana_menu():
-    global ventana_menu
-    ventana_menu = Toplevel(ventana_inicio)
-    ventana_menu.iconbitmap('Proyecto Videojuego Py\imagenes\icono.ico')
-    ventana_menu.title('4 PAISES UNA BANDERA')
-    ventana_menu.geometry('1000x700') # Ancho x Alto
-    ventana_menu.resizable(0,0)
-    
-    Label(ventana_menu, text="Selecciona el Continente", fg="medium blue", font=("Arial",50)).grid(pady=5, row=0, column=2) 
-    Button(ventana_menu, text="Volver al inicio", width=20, command=volver_inicio).grid(pady=10, padx=10, row=2, column=2, )#.place(relx=0.5, rely=0.5)
-    
-    if(ventana_menu):
-        ventana_inicio.withdraw()
-    
-    ventana_menu.mainloop()
-    
-def volver_inicio():
-    ventana_inicio.iconify()
-    ventana_inicio.deiconify()
-    ventana_menu.destroy()
-    
-ventana_inicio()
+
+lbl_Titulo = Label(ventana_Oceania, text="IMAGENES ALEATORIAS:", background="black", fg="white")
+lbl_Titulo.place(x=10, y=10)
+
+
+# --- Imagenes Aleatorias ---
+
+imagenes = ["Proyecto Videojuego Py\imagenes\Banderas\Oceania\Australia.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Fiyi.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Islas-Marshall.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Islas-Solomon.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Kiribati.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Micronesia.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Nauru.png","Proyecto Videojuego Py\imagenes\Banderas\Oceania\Nueva-Zelanda.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Palaos.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Papua-Nueva-Guinea.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Samoa.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Tonga.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Tuvalu.png", "Proyecto Videojuego Py\imagenes\Banderas\Oceania\Vanuatu.png"]
+bandera = random.choice(imagenes)
+foto=PhotoImage(file=bandera)
+lbl_Bandera = Label(ventana_Oceania, image=foto)
+lbl_Bandera.place(x=480, y=100)
+
+# --- Textp de los Botones Aleatorio ---
+paises = ["Australia", "Fiyi", "Islas Marshall", "Islas Solomon", "Kiribati", "Micronesia", "Nauru", "Nueva Zealanda", "Palaos", "Papua Nueva Guinea", "Samoa", "Tonga", "Tuvalu", "Vanuatu"]
+
+texto = random.choice(paises)
+btn_Opcion1 = Button(ventana_Oceania, text=texto, width=35, height=10)
+btn_Opcion1.place(x=30, y=450)
+
+texto = random.choice(paises)
+btn_Opcion2 = Button(ventana_Oceania, text=texto, width=35, height=10)
+btn_Opcion2.place(x=320, y=450)
+
+texto = random.choice(paises)
+btn_Opcion3 = Button(ventana_Oceania, text=texto, width=35, height=10)
+btn_Opcion3.place(x=610, y=450)
+texto = random.choice(paises)
+btn_Opcion4 = Button(ventana_Oceania, text=texto, width=35, height=10)
+btn_Opcion4.place(x=900, y=450)
+ventana_Oceania.mainloop()
